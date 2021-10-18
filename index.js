2f35bbd03fc19e4a4043cd8cab8fc63763f49987
Cristofer <cristoferlorenzana@gmail.com>
2021-10-18 10:44:38 -0500

    commit tree 4939dc4cca9c65e2fb8302fb4bb9f6fb1440489e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const findMatching = function(array, name) {
+    let identifiedDriver = array.filter(function(nameDriver){
+        return nameDriver.toLowerCase() == name.toLowerCase()
+    });
+    return identifiedDriver;
+}
+
+function fuzzyMatch (array, name){
+    return array.filter(match =>
+        match.toLowerCase().indexOf(name.toLowerCase()) === 0);
+    };
+
+
+const matchName = function(array, name){
+    return array.filter(array => array.name === name)
+}
